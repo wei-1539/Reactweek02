573df46a68044d080397de0e61864895af7bf908
RIO WEI <wutony0713@gmail.com>
2023-08-23 00:18:28 +0800

    修改config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 編譯路徑 開發dev 產品build
-  base:process.env.NODE_ENV === 'production'? "/viteTest/":"/",
+  base:process.env.NODE_ENV === 'production'? "/Reactweek02/":"/",
   plugins: [react()],
 })
